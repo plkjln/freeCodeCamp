fc6d9491818429a74537bae0b208014835a361a2
Palak <palakn181@gmail.com>
2018-02-25 13:56:25 -0800

    Updated with different methid

--- freecodecamp/JavaScriptChallenges/DnaPair.js
+++ freecodecamp/JavaScriptChallenges/DnaPair.js
@@ -1,6 +1,5 @@
 
 function pairElement(str) {
-  
     var dnaRes = [];
     var dnaPair = {
       "A": "T",
@@ -8,20 +7,30 @@ function pairElement(str) {
       "C": "G",
       "G": "C"
     };
-   
     for(var i=0;i< str.length; i++){
-      //dna.push(str[i]);
       var dna = [];
       if(dnaPair.hasOwnProperty(str[i])){
         dna.push(str[i]);
         dna.push(dnaPair[str[i]]);
       }
       dnaRes.push(dna);
-      
     }
-    
     return dnaRes;
   }
   
   pairElement("GCG");
   //OUTPUT: [["G", "C"], ["C","G"],["G", "C"]]
+
+
+  //METHOD 2
+function pairElement(str) {
+  //define a map object with all pair possibilities 
+    var map = {T:'A', A:'T', G:'C', C:'G'};
+    //split str into a char Array
+    strArr = str.split('');
+    //replace each Array item with a 2d Array using map
+    for (var i=0;i<strArr.length;i++){
+        strArr[i]=[strArr[i], map[strArr[i]]];
+    }
+    return strArr;
+}
