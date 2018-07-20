e56dae68b670532f6676ca6e50dcaa1365e41960
Palak <palakn181@gmail.com>
2018-07-20 11:18:45 -0700

    JavaScript Algorithms and Data Structures-Roman Numeral Converter

---
+++ freecodecamp/JavaScriptChallenges/RomanNumeralConverter.js
@@ -0,0 +1,30 @@
+/*
+JavaScript Algorithms and Data Structures Projects: Roman Numeral Converter
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+*/
+
+var convertToRoman = function(num) {
+
+    var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+  
+    var romanized = '';
+  
+    for (var index = 0; index < decimalValue.length; index++) {
+      while (decimalValue[index] <= num) {
+        romanized += romanNumeral[index];
+        num -= decimalValue[index];
+      }
+    }
+  
+    return romanized;
+  }
+  
+  // test here
+  convertToRoman(5); // OUTPUT "V".
+
+  /*
+  convertToRoman(83) should return "LXXXIII"
+  */
