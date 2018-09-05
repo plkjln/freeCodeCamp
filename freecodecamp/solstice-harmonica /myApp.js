14cd2a1d9ef6981ddf0c4ba245ee46f597a3b59f
Palak <palakn181@gmail.com>
2018-09-05 12:19:14 -0700

    Get Route Parameter Input from the Client

--- freecodecamp/solstice-harmonica /myApp.js
+++ freecodecamp/solstice-harmonica /myApp.js
@@ -53,6 +53,7 @@ app.use(function middleware(req, res, next) {
 */
 
 /** 8) Chaining middleware. A Time server */
+/*
 app.get('/now', function(req, res, next) {
     req.time = new Date().toString() + 20; 
     res.send({time: req.time});
@@ -60,9 +61,13 @@ app.get('/now', function(req, res, next) {
   }, function(req, res) {
     res.json({time: req.time});  
 });
-
+*/
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", function(req, res) {
+    var word = req.params.word;
+   // respond with the json object
+   res.json({echo: word});
+ });
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
